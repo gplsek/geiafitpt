4aeadb647d97252a1ba953e46c87374f030b7681
parth.dalvi@rishabhsoft.com <parth.dalvi@rishabhsoft.com>
2016-10-04 15:23:44 +0530

    added utility.js for common functions

---
+++ www/js/utility.js
@@ -0,0 +1,19 @@
+angular.module('geiaFitApp').service('utilityService', ['$log',function ($log) {
+    
+    return{
+       
+       calculcateAge : function(dateString){
+            var today = new Date();
+            var birthDate = new Date(dateString);
+            var age = today.getFullYear() - birthDate.getFullYear();
+            var m = today.getMonth() - birthDate.getMonth();
+            if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) 
+                {
+                    age--;
+                }
+                return age;
+        }
+
+    };
+
+}]);
